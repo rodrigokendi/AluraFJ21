ab558bc0a7f3b185706acc53020c8d0c09a5d596
rodrigokendi <rodrigo.kendi@yahoo.com.br>
2018-03-31 11:20:18 -0300

    commit initial web project

---
+++ fj21-agenda/src/br/com/caelum/servlet/OlaMundoServlet.java
@@ -0,0 +1,25 @@
+package br.com.caelum.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/oi")
+public class OlaMundoServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		PrintWriter pw = response.getWriter();
+		pw.println("<html><body><h1>");
+		pw.println("Ola Mundo");
+		pw.println("</h1></body></html>");
+	}
+
+}
